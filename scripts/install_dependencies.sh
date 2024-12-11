b72c755158838cd2319fd2a284bf24a76ff6f971
LAmb-DEX <ambrosio.laura@dreamexchange.onmicrosoft.com>
2024-12-11 14:14:07 +0900

    Create install_dependencies.sh
    Install apache and php in EC2

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e  # Detiene el script si ocurre algún error
+
+# Actualiza el sistema e instala Apache y PHP
+yum update -y
+yum install -y httpd php
+
+# Inicia Apache si no está corriendo
+if ! systemctl is-active --quiet httpd; then
+  systemctl start httpd
+  systemctl enable httpd
+fi
+
+echo "Apache y PHP instalados correctamente."
